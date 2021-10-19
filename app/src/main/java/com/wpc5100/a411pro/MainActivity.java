0c674ce31b8dedfe1281b21c3ffa27fefae5f081
William <jclimp@aol.com>
2021-10-19 16:03:23 -0400

    Updated home page. Created breakfast page. Made link to breakfast page.

--- app/src/main/java/com/wpc5100/a411pro/MainActivity.java
+++ app/src/main/java/com/wpc5100/a411pro/MainActivity.java
@@ -2,7 +2,10 @@ package com.wpc5100.a411pro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button breakfastBtn = (Button)findViewById(R.id.breakfast);
+
+        breakfastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, BreakfastActivity.class));
+            }
+        });
     }
 }
