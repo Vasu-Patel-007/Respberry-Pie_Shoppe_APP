d55bd25973c9bf2c43fd413bd17423695df8cc6f
Luke <lps5497@psu.edu>
2021-10-31 15:10:42 -0400

    Luke - Added a dropdown menu (not fully realized) buttons got rearranged on the menu page but it compiles

--- app/src/main/java/com/wpc5100/a411pro/MainActivity.java
+++ app/src/main/java/com/wpc5100/a411pro/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +32,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, coffee.class));
             }
         });
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.Breakfast);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter((myAdapter));
     }
 }
