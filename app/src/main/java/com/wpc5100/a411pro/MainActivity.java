62fc979ce04b9f5ce19bd1bd617e4c3b2d5b822e
Vasu Patel <patelvasu711@gmail.com>
2021-11-05 20:31:28 -0400

    I have worked on Calender, so that user can select the date from the calander. I worked on "build.gradle." I worked on MainActivity.java and I have created activity_payment_page.

--- app/src/main/java/com/wpc5100/a411pro/MainActivity.java
+++ app/src/main/java/com/wpc5100/a411pro/MainActivity.java
@@ -2,20 +2,31 @@ package com.wpc5100.a411pro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    // date_button
+    Button select_date;
+    TextView date;
+    DatePickerDialog date_picker_screen;
+    int year;
+    int month;
+    int day_of_month;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +54,31 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
+
+
+
         });
+                    // code for date_picking_button_starts here
+            select_date = findViewById(R.id.btndate);
+            date = findViewById(R.id.selected_date);
 
+            select_date.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    calendar = Calendar.getInstance();
+                    year = calendar.get(Calendar.YEAR);
+                    month = calendar.get(Calendar.MONTH);
+                    day_of_month = calendar.get(Calendar.DAY_OF_MONTH);
+                    date_picker_screen = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                            date.setText(month + "/" + year);
 
+                        }
+                    }, 0,0,0);
+                    date_picker_screen.show();
+                }
+            });
 
         //Coffee Menu
         Spinner coffeeSpinner = (Spinner) findViewById(R.id.coffee_menu);
@@ -62,5 +95,7 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.pastriesNames));
         pastriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         pastriesSpinner.setAdapter((pastriesAdapter));
+
+
     }
 }
