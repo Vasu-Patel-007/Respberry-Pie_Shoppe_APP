b4e0fbdadd550231a00f3d9f49079db12de65418
William <jclimp@aol.com>
2021-12-08 20:17:24 -0500

    Changed inc and dec buttons, started budget page

--- app/src/main/java/com/wpc5100/a411pro/MainActivity.java
+++ app/src/main/java/com/wpc5100/a411pro/MainActivity.java
@@ -800,6 +800,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button budgetBtn = (Button) findViewById(R.id.budget);
+        budgetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, activity_budget.class));
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/wpc5100/a411pro/activity_budget.java
@@ -0,0 +1,14 @@
+package com.wpc5100.a411pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_budget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget);
+    }
+}
